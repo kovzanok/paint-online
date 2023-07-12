23cde553c210977c703681f89d739c61a68529bd
Alex Kovzan <alexanderkovzan@gmail.com>
2023-07-12 20:43:39 +0300

    feat: add store to provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import Canvas from "./components/Canvas";
 import SettingsBar from "./components/SettingsBar";
-import ToolBar from "./components/Toolbar";
+import ToolBar from "./components/ToolBar";
 import "./style/App.scss";
+import { Provider } from "react-redux";
 
+import { store } from "./store/store";
 function App() {
   return (
     <>
-      <ToolBar />
-      <SettingsBar />
-      <Canvas />
+      <Provider store={store}>
+        <ToolBar />
+        <SettingsBar />
+        <Canvas />
+      </Provider>
     </>
   );
 }
