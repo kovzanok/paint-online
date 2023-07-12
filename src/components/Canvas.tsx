7239d93108e19975fdf647e18ed50b19fd585350
Alex Kovzan <alexanderkovzan@gmail.com>
2023-07-12 19:59:30 +0300

    feat: add canvas component

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,5 +1,9 @@
 import "../style/Canvas.scss";
 
 export default function Canvas() {
-  return <div>Canvas</div>;
+  return (
+    <div className='canvas'>
+      <canvas width={600} height={600} />
+    </div>
+  );
 }
