7cba67518dadbcf460747f300fbaba00d94150eb
Alex Kovzan <alexanderkovzan@gmail.com>
2023-07-12 19:54:00 +0300

    feat: add settings bar component

--- src/components/SettingsBar.tsx
+++ src/components/SettingsBar.tsx
@@ -1,5 +1,22 @@
 import "../style/SettingsBar.scss";
 
 export default function SettingsBar() {
-  return <div>SettingsBar</div>;
+  return (
+    <div className="settingsbar">
+      <ul className="settingsbar__list">
+        <li>
+          <label className="settingsbar__label">
+            Line weight:
+            <input min={1} max={50} defaultValue={1} type='number' />
+          </label>
+        </li>
+        <li>
+          <label className="settingsbar__label">
+            Stroke color:
+            <input type='color' />
+          </label>
+        </li>
+      </ul>
+    </div>
+  );
 }
