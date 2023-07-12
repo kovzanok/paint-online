ad10b616b28c414bc78f9edd5c9362ac49179fc7
Alex Kovzan <alexanderkovzan@gmail.com>
2023-07-12 19:45:42 +0300

    feat: add toolbar component

--- src/components/ToolBar.tsx
+++ src/components/ToolBar.tsx
@@ -1,5 +1,46 @@
 import "../style/Toolbar.scss";
+import BrushIcon from "../assets/BrushIcon.tsx";
+import RectIcon from "../assets/RectIcon.tsx";
+import CircleIcon from "../assets/CircleIcon.tsx";
+import CustomButton from "./CustomButton.tsx";
 
 export default function ToolBar() {
-  return <div>Toolbar</div>;
+  return (
+    <div className='toolbar'>
+      <ul className='toolbar__list'>
+        <li>
+          <CustomButton>
+            <BrushIcon />
+          </CustomButton>
+        </li>
+        <li>
+          <CustomButton>
+            <RectIcon />
+          </CustomButton>
+        </li>
+        <li>
+          <CustomButton>
+            <CircleIcon />
+          </CustomButton>
+        </li>
+        <li>
+          <CustomButton className='button_erase' />
+        </li>
+        <li>
+          <input type='color' />
+        </li>
+      </ul>
+      <ul className='toolbar__list'>
+        <li>
+          <CustomButton className='button_undo' />
+        </li>
+        <li>
+          <CustomButton className='button_redo' />
+        </li>
+        <li>
+          <CustomButton className='button_save' />
+        </li>
+      </ul>
+    </div>
+  );
 }
