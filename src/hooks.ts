76cef175592fb3b165a944d245ce8e515f6dc314
Alex Kovzan <alexanderkovzan@gmail.com>
2023-07-14 00:02:26 +0300

    feat: add update tool hook

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,7 +1,23 @@
 import { useDispatch, useSelector } from "react-redux";
 import type { TypedUseSelectorHook } from "react-redux";
 import type { RootState, AppDispatch } from "./store/store";
+import Tool from "./tools/Tool";
+import { useContext } from "react";
+import { ToolContext } from "./context";
 
+type ToolConstructorType = new (toolParams: ToolParams) => Tool;
+
+const getToolConstuctor = (tool: Tool): ToolConstructorType =>
+  Object.getPrototypeOf(tool).constructor;
 
 export const useAppDispatch: () => AppDispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+export const useToolUpdate = () => {
+  const { tool, setTool } = useContext(ToolContext);
+  const toolConstructor = tool && getToolConstuctor(tool);
+  const updateTool = (toolParams: ToolParams) => {
+    if (toolConstructor) setTool(new toolConstructor(toolParams));
+  };
+  return updateTool;
+};
