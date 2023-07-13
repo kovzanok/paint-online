8c5ff2118407403d2e432bf3dcacc4827e213a92
Alex Kovzan <alexanderkovzan@gmail.com>
2023-07-13 18:53:56 +0300

    feat: add erase class

---
+++ src/tools/Erase.ts
@@ -0,0 +1,33 @@
+import Tool from "./Tool";
+
+export default class Erase extends Tool {
+  constructor(canvas: HTMLCanvasElement) {
+    super(canvas);
+
+    this.canvas.onmousedown = this.handleMouseDown.bind(this);
+    this.canvas.onmousemove = this.handleMove.bind(this);
+    this.canvas.onmouseup = () => {
+      this.isMouseDown = false;
+    };
+    if (this.ctx) {
+      this.ctx.strokeStyle = "white";
+    }
+  }
+
+  private handleMouseDown(e: MouseEvent) {
+    this.isMouseDown = true;
+    this.ctx?.beginPath();
+    this.ctx?.moveTo(e.offsetX, e.offsetY);
+  }
+
+  private handleMove(e: MouseEvent) {
+    if (this.isMouseDown) {
+      this.draw(e.offsetX, e.offsetY);
+    }
+  }
+
+  private draw(x: number, y: number) {
+    this.ctx?.lineTo(x, y);
+    this.ctx?.stroke();
+  }
+}
