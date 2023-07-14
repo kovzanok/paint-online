7bc03a5c591f469f386cd87eb0a4d1f44575f95b
Alex Kovzan <alexanderkovzan@gmail.com>
2023-07-14 10:35:47 +0300

    feat: save canvas state on click

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -3,22 +3,37 @@ import { useRef, useEffect, useContext } from "react";
 import "../style/Canvas.scss";
 import { CanvasContext, ToolContext } from "../context";
 import Brush from "../tools/Brush";
+import { useAppSelector } from "../hooks";
 
 export default function Canvas() {
-  const { setCanvas } = useContext(CanvasContext);
+  const { canvas, setCanvas, undoList, setUndoList } =
+    useContext(CanvasContext);
   const { setTool } = useContext(ToolContext);
-
+  const settings = useAppSelector((state) => state.settings);
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
 
   useEffect(() => {
     if (setCanvas && canvasRef.current && setTool) {
       setCanvas(canvasRef.current);
-      setTool(new Brush(canvasRef.current));
+      setTool(new Brush({ canvas: canvasRef.current, ...settings }));
     }
   }, []);
+
+  const mouseDonwHandler = () => {
+    if (canvas) {
+      const imgSrc = canvas.toDataURL();
+      setUndoList([...undoList, imgSrc]);
+    }
+  };
+
   return (
     <div className='canvas'>
-      <canvas ref={canvasRef} width={600} height={600} />
+      <canvas
+        onMouseDown={mouseDonwHandler}
+        ref={canvasRef}
+        width={600}
+        height={600}
+      />
     </div>
   );
 }
