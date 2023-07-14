3ad8fde626cc899e06bb06cd75278c93c74177f2
Alex Kovzan <alexanderkovzan@gmail.com>
2023-07-14 15:40:55 +0300

    feat: add handlers

--- back/index.js
+++ back/index.js
@@ -1,18 +1,24 @@
 const express = require("express");
-
+const path = require("path");
+const fs = require("fs/promises");
 const app = express();
 const WSServer = require("express-ws")(app);
 const aWss = WSServer.getWss();
 const PORT = process.env.PORT | 3000;
 
+const socketsImage = {};
+
 app.ws("/", (ws, req) => {
   ws.on("message", (msg) => {
     const message = JSON.parse(msg);
+
     switch (message.method) {
       case "connection":
         conntectionHandler(ws, message);
         break;
-
+      case "draw":
+        drawHandler(ws, message);
+        break;
       default:
         console.log("Не найден ни один метод");
         break;
@@ -27,12 +33,35 @@ app.listen(PORT, () => {
 function conntectionHandler(ws, message) {
   ws.id = message.id;
   broadcastConnection(ws, message);
+  sendCurrentImage(ws, message);
 }
 
 function broadcastConnection(ws, message) {
   aWss.clients.forEach((client) => {
     if (client.id === message.id) {
-      client.send(`Текущий пользователь: ${message.userName}`);
+      client.send(JSON.stringify({ method: "connection", success: true }));
     }
   });
+}
+
+function sendCurrentImage(ws, message) {
+  const imageUrl = socketsImage[ws.id];
+  if (imageUrl) ws.send(JSON.stringify({ method: "draw", url: imageUrl }));
+}
+
+function drawHandler(ws, message) {
+  saveCurrentImage(ws, message);
+  broadcastDraw(ws, message);
+}
+
+function broadcastDraw(ws, message) {
+  aWss.clients.forEach((client) => {
+    if (client.id === message.id) {
+      client.send(JSON.stringify({ method: "draw", url: message.url }));
+    }
+  });
+}
+
+function saveCurrentImage(ws, message) {
+  socketsImage[ws.id] = message.url;
 }
