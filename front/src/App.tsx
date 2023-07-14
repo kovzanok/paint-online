624a86bde9cc22e5d638c246cc6ea28b61a599a5
Alex Kovzan <alexanderkovzan@gmail.com>
2023-07-14 14:46:55 +0300

    feat: remove rounting to main.tsx

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -7,69 +7,28 @@ import { CanvasContext, ToolContext } from "./context";
 import { store } from "./store/store";
 import { useState } from "react";
 import Tool from "./tools/Tool";
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 
 function App() {
   const [canvas, setCanvas] = useState<HTMLCanvasElement | null>(null);
   const [tool, setTool] = useState<Tool | null>(null);
   const [socket, setSocket] = useState<WebSocket | null>(null);
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route
-            path='/:id'
-            element={
-              <ToolContext.Provider value={{ tool, setTool }}>
-                <CanvasContext.Provider
-                  value={{
-                    canvas,
-                    setCanvas,
-                    undoList,
-                    setUndoList,
-                    redoList,
-                    setRedoList,
-                  }}
-                >
-                  <Provider store={store}>
-                    <ToolBar />
-                    <SettingsBar />
-                    <Canvas />
-                  </Provider>
-                </CanvasContext.Provider>
-              </ToolContext.Provider>
-            }
-          />
-          <Route
-            path='*'
-            element={
-              <>
-                {" "}
-                <ToolContext.Provider value={{ tool, setTool }}>
-                  <CanvasContext.Provider
-                    value={{
-                      canvas,
-                      setCanvas,
-                      undoList,
-                      setUndoList,
-                      redoList,
-                      setRedoList,
-                    }}
-                  >
-                    <Provider store={store}>
-                      <ToolBar />
-                      <SettingsBar />
-                      <Canvas />
-                    </Provider>
-                  </CanvasContext.Provider>
-                </ToolContext.Provider>
-                <Navigate to={`${Date.now().toString(16)}`} replace />
-              </>
-            }
-          />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <ToolContext.Provider value={{ tool, setTool }}>
+      <CanvasContext.Provider
+        value={{
+          canvas,
+          setCanvas,
+          socket,
+          setSocket,
+        }}
+      >
+        <Provider store={store}>
+          <ToolBar />
+          <SettingsBar />
+          <Canvas />
+        </Provider>
+      </CanvasContext.Provider>
+    </ToolContext.Provider>
   );
 }
 
--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path='/:id' element={<App />} />
+        <Route
+          path='/'
+          element={
+            <>
+              <App />
+              <Navigate to={`${Date.now().toString(16)}`} replace />
+            </>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
