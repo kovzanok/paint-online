d6e4cf7ffd64a0efc0b09cca0bc366c4a4a9272c
Alex Kovzan <alexanderkovzan@gmail.com>
2023-07-14 14:48:03 +0300

    feat: add web socket connection

--- front/src/components/Canvas.tsx
+++ front/src/components/Canvas.tsx
@@ -1,13 +1,16 @@
 import { useRef, useEffect, useContext } from "react";
-
 import "../style/Canvas.scss";
 import { CanvasContext, ToolContext } from "../context";
 import Brush from "../tools/Brush";
-import { useAppSelector } from "../hooks";
+import { useAppDispatch, useAppSelector } from "../hooks";
+import { useParams } from "react-router-dom";
+import { updateCanvasImage } from "../utils";
+import { addToUndoList } from "../store/slices/CanvasImgSlice";
 
 export default function Canvas() {
-  const { canvas, setCanvas, undoList, setUndoList } =
-    useContext(CanvasContext);
+  const { canvas, setCanvas, socket, setSocket } = useContext(CanvasContext);
+  const { id } = useParams();
+  const dispatch = useAppDispatch();
   const { setTool } = useContext(ToolContext);
   const settings = useAppSelector((state) => state.settings);
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
@@ -19,17 +22,50 @@ export default function Canvas() {
     }
   }, []);
 
-  const mouseDonwHandler = () => {
+  useEffect(() => {
+    const socket = new WebSocket("ws://localhost:3000");
+    setSocket(socket);
+    socket.onopen = () => {
+      socket.send(
+        JSON.stringify({
+          method: "connection",
+          id,
+        })
+      );
+    };
+    socket.onmessage = (event) => {
+      const message = JSON.parse(event.data);
+      switch (message.method) {
+        case "connection":
+          break;
+        case "draw":
+          const url = message.url;
+          if (canvasRef.current) updateCanvasImage(url, canvasRef.current);
+          break;
+        default:
+          break;
+      }
+    };
+  }, []);
+
+  const mouseDownHandler = () => {
     if (canvas) {
-      const imgSrc = canvas.toDataURL();
-      setUndoList([...undoList, imgSrc]);
+      dispatch(addToUndoList(canvas.toDataURL()));
+    }
+  };
+
+  const mouseUpHandler = () => {
+    if (canvas) {
+      const imgUrl = canvas.toDataURL();
+      socket?.send(JSON.stringify({ method: "draw", id, url: imgUrl }));
     }
   };
 
   return (
     <div className='canvas'>
       <canvas
-        onMouseDown={mouseDonwHandler}
+        onMouseDown={mouseDownHandler}
+        onMouseUp={mouseUpHandler}
         ref={canvasRef}
         width={600}
         height={600}
